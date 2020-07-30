67f8627220f7a8be091971487e23dda81539b47d
williamwjd <william.jose.wjd@gmail.com>
2020-07-30 09:24:02 -0300

    provider envio de e-mail

--- src/useCases/CreateUser/CreateUserUseCase.ts
+++ src/useCases/CreateUser/CreateUserUseCase.ts
@@ -1,9 +1,13 @@
 import { IUsersRepository } from '../../repositories/IUsersRepository';
 import { ICreateUserRequestDTO } from './CreateUserDTO';
 import { User } from '../../entities/User';
+import { IMailProvider } from '../../providers/IMailProvider';
 
 export class CreateUserUseCase {
-  constructor(private usersRepository: IUsersRepository) {}
+  constructor(
+    private usersRepository: IUsersRepository,
+    private mailProvider: IMailProvider
+  ) {}
 
   async execute(data: ICreateUserRequestDTO) {
     const userAreadyExists = await this.usersRepository.findByEmail(data.email);
@@ -15,5 +19,18 @@ export class CreateUserUseCase {
     const user = new User(data);
 
     await this.usersRepository.save(user);
+
+    this.mailProvider.sendMail({
+      to: {
+        name: data.name,
+        email: data.email,
+      },
+      from: {
+        name: 'Equipe will',
+        email: 'equipe@will.com.br',
+      },
+      subject: 'Seja bem vindo à plataforma',
+      body: '<p>Você já pode fazer login em nossa plataforma</p>',
+    });
   }
 }
